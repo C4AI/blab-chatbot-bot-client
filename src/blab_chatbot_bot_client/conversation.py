023544718d58b7afa2bc627e9e58e9463805cd54
Vinícius <matosvb@gmail.com>
2023-04-11 17:45:17 -0300

    Add support to manager commands

--- src/blab_chatbot_bot_client/conversation.py
+++ src/blab_chatbot_bot_client/conversation.py
@@ -1,10 +1,15 @@
 from __future__ import annotations
 
+import json
 from queue import Queue
 from typing import Any
 from uuid import uuid4
 
-from blab_chatbot_bot_client.data_structures import OutgoingMessage, Message
+from blab_chatbot_bot_client.data_structures import (
+    OutgoingMessage,
+    Message,
+    MessageType,
+)
 from blab_chatbot_bot_client.settings_format import BlabBotClientSettings
 
 
@@ -40,3 +45,25 @@ class BotClientConversation:
     @classmethod
     def generate_local_id(cls) -> str:
         return str(uuid4()).replace("-", "")
+
+    @classmethod
+    def generate_approval_command(
+        cls, message_id: str, optional_text: str = ""
+    ) -> OutgoingMessage:
+        return OutgoingMessage(
+            type=MessageType.TEXT,
+            text=optional_text,
+            command=json.dumps(dict(action="approve"), quoted_message_id=message_id),
+            local_id=cls.generate_local_id(),
+        )
+
+    @classmethod
+    def generate_redirection_command(
+        cls, message_id: str, optional_text=""
+    ) -> OutgoingMessage:
+        return OutgoingMessage(
+            type=MessageType.TEXT,
+            text=optional_text,
+            command=json.dumps(dict(action="approve"), quoted_message_id=message_id),
+            local_id=cls.generate_local_id(),
+        )
